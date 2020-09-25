00840643bf363501fb80030b2929aa20daa4e2d9
lnk2333 <60033324+lnk2333@users.noreply.github.com>
2020-09-25 19:01:24 +0800

    语法树

---
+++ test/test1.c
@@ -0,0 +1,8 @@
+extern int puts(char * s);
+
+int main()
+{
+    puts("Hello\n");
+    puts("World\n");
+    return 0;
+}
---
+++ test/test2.c
@@ -0,0 +1,25 @@
+int fuck(int x)
+{
+    int y = 0;
+    return x + y;
+}
+
+int main()
+{
+    int a = fuck(1);
+    int abc;
+    int i;
+    i=2;
+    while (1) {
+        abc = abc + 1;
+        if (abc > 1) {
+
+            if (abc > 10) {
+                a = a+1;
+                break;
+            }
+        }
+
+	}
+    return 0;
+}
